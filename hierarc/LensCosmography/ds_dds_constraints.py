a57833b2c239a67be72fd263a068e94883170c65
sibirrer <sibirrer@gmail.com>
2020-02-12 22:39:22 -0800

    sampling lenses with kinematics routines extended

--- hierarc/LensCosmography/ds_dds_constraints.py
+++ hierarc/LensCosmography/ds_dds_constraints.py
@@ -21,7 +21,7 @@ class DsDdsConstraints(object):
         :param gamma_error: 1-sigma uncertainty on power-law slope
         :param r_eff: half-light radius of the deflector (arc seconds)
         :param r_eff_error: uncertainty on half-light radius
-        :param sigma_v: velocity dispersion of the main deflecto in km/s
+        :param sigma_v: velocity dispersion of the main deflector in km/s
         :param sigma_v_error: 1-sigma uncertainty in velocity dispersion
         :param kwargs_aperture: spectroscopic aperture keyword arguments, see lenstronomy.Galkin.aperture for options
         :param kwargs_seeing: seeing condition of spectroscopic observation, corresponds to kwargs_psf in the GalKin module specified in lenstronomy.GalKin.psf
@@ -48,22 +48,28 @@ class DsDdsConstraints(object):
                                                     Hernquist_approx=hernquist_approx, MGE_light=MGE_light,
                                                     MGE_mass=False, kwargs_mge_light=kwargs_mge_light)
         self._kwargs_lens_light = kwargs_lens_light
+        self._anisotropy_model = anisotropy_model
 
-    def draw_vel_disp(self, num=1):
+    def draw_vel_disp(self, num=1, no_error=False):
         """
         produces realizations of measurements based on the uncertainty in the measurement of the velocity dispersion
 
         :param num: int, number of realization
+        :param no_error: bool, if True, does not render from the uncertainty but uses the mean values instead
         :return: realizations of draws from the measured velocity dispersion
         """
+        if no_error is True:
+            return self._sigma_v
         return np.random.normal(loc=self._sigma_v, scale=self._sigma_v_error, size=num)
 
-    @property
-    def draw_lens(self):
+    def draw_lens(self, no_error=False):
         """
 
+        :param no_error: bool, if True, does not render from the uncertainty but uses the mean values instead
         :return: theta_E, gamma, r_eff
         """
+        if no_error is True:
+            return self._theta_E, self._gamma, self._r_eff
         theta_E_draw = np.random.normal(loc=self._theta_E, scale=self._theta_E_error)
         gamma_draw = np.random.normal(loc=self._gamma, scale=self._gamma_error)
         r_eff_draw = np.random.normal(loc=self._r_eff, scale=self._r_eff_error)
@@ -78,31 +84,97 @@ class DsDdsConstraints(object):
         """
 
         # compute dimensionless kinematic quantity
-        if no_error is True:
-            theta_E_draw, gamma_draw, r_eff_draw, sigma_v_draw = self._theta_E, self._gamma, self._r_eff, self._sigma_v
-        else:
-            theta_E_draw, gamma_draw, r_eff_draw = self.draw_lens
-            sigma_v_draw = self.draw_vel_disp(num=1)
+        j_kin = self.j_kin_draw(kwargs_anisotropy, no_error)
+        sigma_v_draw = self.draw_vel_disp(num=1, no_error=no_error)
+        ds_dds = self._td_cosmo.Ds_Dds_from_kinematics(sigma_v_draw, j_kin, kappa_s=0, kappa_ds=0)
+        return ds_dds
+
+    def j_kin_draw(self, kwargs_anisotropy, no_error=False):
+        """
+        one simple sampling realization of the dimensionless kinematics of the model
+
+        :param kwargs_anisotropy: keyword argument of anisotropy setting
+        :param no_error: bool, if True, does not render from the uncertainty but uses the mean values instead
+        :return: dimensionless kinematic component J() Birrer et al. 2016, 2019
+        """
+        theta_E_draw, gamma_draw, r_eff_draw = self.draw_lens(no_error=no_error)
         kwargs_lens = [{'theta_E': theta_E_draw, 'gamma': gamma_draw, 'center_x': 0, 'center_y': 0}]
         if self._kwargs_lens_light is None:
             kwargs_light = [{'Rs': r_eff_draw * 0.551, 'amp': 1.}]
         else:
             kwargs_light = self._kwargs_lens_light
-        J = self._td_cosmo.velocity_dispersion_dimension_less(kwargs_lens=kwargs_lens, kwargs_lens_light=kwargs_light,
-                                                        kwargs_anisotropy=kwargs_anisotropy, r_eff=r_eff_draw,
-                                                        theta_E=theta_E_draw, gamma=gamma_draw)
-        ds_dds = self._td_cosmo.Ds_Dds_from_kinematics(sigma_v_draw, J, kappa_s=0, kappa_ds=0)
-        return ds_dds
+        j_kin = self._td_cosmo.velocity_dispersion_dimension_less(kwargs_lens=kwargs_lens,
+                                                                  kwargs_lens_light=kwargs_light,
+                                                                  kwargs_anisotropy=kwargs_anisotropy, r_eff=r_eff_draw,
+                                                                  theta_E=theta_E_draw, gamma=gamma_draw)
+        return j_kin
 
-    def ds_dds_sample(self, num_sample, kwargs_anisotropy):
+    def ds_dds_sample(self, kwargs_anisotropy, num_sample_model=20, num_kin_measurements=50):
         """
 
-        :param num_sample: int, number of samples drawn from the measurement uncertainties
-        :param kwargs_anisotropy:
+        :param num_sample_model: number of samples drawn from the lens and light model posterior to compute the dimensionless
+        kinematic component J()
+        :param num_kin_measurements: number of draws from the velocity dispersion measurements to simple sample the
+        posterior in Ds/Dds. The total number of posteriors is num_sample_model x num_kin_measurements
+        :param kwargs_anisotropy: keyword argument with stellar anisotropy configuration parameters
         :return: numpy array of posterior values of Ds/Dds
         """
         ds_dds_list = []
-        for i in range(num_sample):
-            ds_dds = self.ds_dds_realization(kwargs_anisotropy)
-            ds_dds_list.append(ds_dds)
+        for i in range(num_sample_model):
+            j_kin = self.j_kin_draw(kwargs_anisotropy, no_error=False)
+            for j in range(num_kin_measurements):
+                sigma_v_draw = self.draw_vel_disp(num=1, no_error=False)
+                ds_dds = self._td_cosmo.Ds_Dds_from_kinematics(sigma_v_draw, j_kin, kappa_s=0, kappa_ds=0)
+                ds_dds_list.append(ds_dds)
         return np.array(ds_dds_list)
+
+    def hierarchy_configuration(self, num_sample_model=20, num_kin_measurements=50):
+        """
+        routine to configure the likelihood to be used in the hierarchical sampling. In particular, a default
+        configuration is set to compute the Gaussian approximation of Ds/Dds by sampling the posterior and the estimate
+        of the variance of the sample. The anisotropy scaling is then performed. Different anisotropy models are
+        supported.
+
+        :param num_sample_model: number of samples drawn from the lens and light model posterior to compute the dimensionless
+        kinematic component J()
+        :param num_kin_measurements: number of draws from the velocity dispersion measurements to simple sample the
+        posterior in Ds/Dds. The total number of posteriors is num_sample_model x num_kin_measurements
+        :return: keyword arguments
+        """
+        # here we set the default anisotropy model values and the range to be probed
+        if self._anisotropy_model == 'OsipkovMerritt':
+            ani_param_array = np.linspace(0.1, 5, 20)  # used for r_ani OsipkovMerritt anisotropy discription
+            a_ani_0 = 1
+            r_ani = a_ani_0 * self._r_eff
+            kwargs_anisotropy_0 = {'r_ani': r_ani}
+        else:
+            ani_param_array = np.linspace(0, 1, 10)  # used for constant anisotropy description
+            a_ani_0 = 0.1
+            kwargs_anisotropy_0 = {'beta': a_ani_0}
+
+        # here we simple sampling the default anisotropy configuration and compute the mean and std of the sample
+        ds_dds_sample = self.ds_dds_sample(kwargs_anisotropy=kwargs_anisotropy_0, num_sample_model=num_sample_model,
+                                           num_kin_measurements=num_kin_measurements)
+        ds_dds_mean = np.mean(ds_dds_sample)
+        ds_dds_sigma = np.std(ds_dds_sample)
+
+        # here we loop through the possible anisotropy configuration within the model parameterization
+        ds_dds_ani_0 = self.ds_dds_realization(kwargs_anisotropy_0, no_error=True)
+        ani_scaling_array = []
+        for a_ani in ani_param_array:
+            if self._anisotropy_model == 'OsipkovMerritt':
+                r_ani = a_ani * self._r_eff
+                kwargs_anisotropy = {'r_ani': r_ani}
+            elif self._anisotropy_model == 'const':
+                kwargs_anisotropy = {'beta': a_ani}
+            else:
+                raise ValueError('anisotropy model %s not supported.' % self._anisotropy_model)
+            ds_dds_ani = self.ds_dds_realization(kwargs_anisotropy, no_error=True)
+            ani_scaling_array.append(ds_dds_ani / ds_dds_ani_0)
+        ani_scaling_array = np.array(ani_scaling_array)
+
+        # configuration keyword arguments for the hierarchical sampling
+        kwargs_likelihood = {'z_lens': self._td_cosmo._z_lens, 'z_source': self._td_cosmo._z_source,
+                             'ds_dds_mean': ds_dds_mean,  'ds_dds_sigma': ds_dds_sigma,
+                             'ani_param_array': ani_param_array, 'ani_scaling_array': ani_scaling_array}
+        return kwargs_likelihood
