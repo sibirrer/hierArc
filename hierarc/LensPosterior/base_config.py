model covariance matrix fixed

hierarc/LensPosterior/base_config.py
@@ -52,7 +52,7 @@ class BaseLensConfig(TDCosmography, ImageModelPosterior):
         self._anisotropy_model = anisotropy_model
 
         if self._anisotropy_model == 'OsipkovMerritt':
-            self._ani_param_array = np.linspace(0.1, 5, 20)  # used for r_ani OsipkovMerritt anisotropy description
+            self._ani_param_array = np.array([0.1, 0.2, 0.5, 1, 2, 5])  # used for r_ani OsipkovMerritt anisotropy description
         elif self._anisotropy_model == 'const':
             self._ani_param_array = np.linspace(0, 1, 10)  # used for constant anisotropy description
         else:

hierarc/Likelihood/LensLikelihood/ifu_kin_likelihood.py
@@ -40,5 +40,6 @@ class IFUKinCov(AnisotropyScalingIFU):
         scaling_ifu = self.ani_scaling(a_ani)
         sigma_v_predict = np.sqrt(ds_dds * scaling_ifu * self._j_mean_list) * const.c / 1000
         delta = self._sigma_v_measured - sigma_v_predict
-        cov_error = np.linalg.inv(self._error_cov_measurement + self._error_cov_j_sqrt * ds_dds * scaling_ifu * (const.c / 1000)**2)
+        scaling_matix = np.outer(np.sqrt(scaling_ifu), np.sqrt(scaling_ifu))
+        cov_error = np.linalg.inv(self._error_cov_measurement + self._error_cov_j_sqrt * scaling_matix * ds_dds * (const.c / 1000)**2)
         return -delta.dot(cov_error.dot(delta)) / 2.