2d375b8c87d107e3ae74637b59ce34abc4ad830f
jiwoncpark <jiwon.christine.park@gmail.com>
2020-03-30 15:01:52 -0700

    Enable lognorm likelihood for D_dt, floating-point comparison to scipy passing

--- hierarc/Likelihood/lens_likelihood.py
+++ hierarc/Likelihood/lens_likelihood.py
@@ -33,6 +33,8 @@ class LensLikelihoodBase(object):
             self._lens_type = TDKinGaussian(z_lens, z_source, **kwargs_likelihood)
         elif likelihood_type in ['KinGaussian']:
             self._lens_type = KinLikelihoodGaussian(z_lens, z_source, **kwargs_likelihood)
+        elif likelihood_type == 'TDLogNorm':
+            self._lens_type = TDLikelihoodLogNorm(z_lens, z_source, **kwargs_likelihood)
         elif likelihood_type == 'TDKinSkewLogNorm':
             self._lens_type = TDKinLikelihoodSklogn(z_lens, z_source, **kwargs_likelihood)
         elif likelihood_type == 'TDSkewLogNorm':
@@ -122,7 +124,6 @@ class TDKinLikelihoodKDE(object):
             return self._interp_log_likelihood(dd_, ddt)[0]
         return self._kde_likelihood.logLikelihood(dd_, ddt)[0]
 
-
 class TDKinLikelihoodSklogn(object):
     """
     class for evaluating the 2-d posterior of Ddt vs Dd coming from a lens with time delays and kinematics measurement
@@ -196,7 +197,7 @@ class TDLikelihoodSklogn(object):
 class TDLikelihoodGaussian(object):
     """
     class to handle cosmographic likelihood coming from modeling lenses with imaging and kinematic data but no time delays.
-    Thus Ddt is not constraint but the kinematics can constrain Ds/Dds
+    Thus Ddt is not constrained but the kinematics can constrain Ds/Dds
 
     The current version includes a Gaussian in Ds/Dds but can be extended.
     """
@@ -223,11 +224,39 @@ class TDLikelihoodGaussian(object):
         """
         return - (ddt - self._ddt_mean) ** 2 / self._ddt_sigma2 / 2
 
+class TDLikelihoodLogNorm(object):
+    """
+    The cosmographic likelihood coming from modeling lenses with imaging and kinematic data but no time delays, where the form of the likelihood is a lognormal distribution.
+    Thus Ddt is not constrained but the kinematics can constrain Ds/Dds
+
+    The current version includes a Gaussian in Ds/Dds but can be extended.
+    """
+    def __init__(self, z_lens, z_source, ddt_mu, ddt_sigma):
+        """
+        :param z_lens: lens redshift
+        :param z_source: source redshift
+        :param ddt_mean: mean of log(Ddt distance)
+        :param ddt_sigma: 1-sigma uncertainty in the log(Ddt distance)
+        """
+        self._z_lens = z_lens
+        self._ddt_mu = ddt_mu
+        self._ddt_sigma2 = ddt_sigma ** 2
+
+    def log_likelihood(self, ddt, dd=None, aniso_scaling=None):
+        """
+        Note: kinematics + imaging data can constrain Ds/Dds. The input of Ddt, Dd is transformed here to match Ds/Dds
+
+        :param ddt: time-delay distance
+        :param dd: angular diameter distance to the deflector
+        :param aniso_scaling: numpy array of anisotropy scaling on prediction of Ds/Dds
+        :return: log likelihood given the single lens analysis
+        """
+        return -0.5*(np.log(ddt) - self._ddt_mu)**2/self._ddt_sigma2 - np.log(ddt) - 0.5*np.log(self._ddt_sigma2)
 
 class KinLikelihoodGaussian(object):
     """
     class to handle cosmographic likelihood coming from modeling lenses with imaging and kinematic data but no time delays.
-    Thus Ddt is not constraint but the kinematics can constrain Ds/Dds
+    Thus Ddt is not constrained but the kinematics can constrain Ds/Dds
 
     The current version includes a Gaussian in Ds/Dds but can be extended.
     """
