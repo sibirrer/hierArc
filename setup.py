6561126d21922a3323c0e59b9f40b2ab7875cb0f
sibirrer <sibirrer@gmail.com>
2020-05-01 11:22:23 -0700

    setup.py and test updates

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 """The setup script."""
-
+import os
+import sys
+from setuptools.command.test import test as TestCommand
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -10,11 +12,26 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
 requirements = ['numpy>=1.13', 'scipy>=0.14.0']
 
 setup_requirements = ['pytest-runner']
 
 test_requirements = ['pytest>=3', ]
+
+PACKAGE_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
 
 setup(
     author="Simon Birrer",
@@ -38,11 +55,13 @@ setup(
     include_package_data=True,
     keywords='hierarc',
     name='hierarc',
-    packages=find_packages(include=['hierarc', 'hierarc.*']),
+    packages=find_packages(PACKAGE_PATH, "test"),
+    #packages=find_packages(include=['hierarc', 'hierarc.*']),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite='test',
     tests_require=test_requirements,
     url='https://github.com/sibirrer/hierarc',
     version='0.1.0',
     zip_safe=False,
+    cmdclass={'test': PyTest}
 )
