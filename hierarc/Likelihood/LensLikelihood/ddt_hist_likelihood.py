130ca908546ba8f8afdfb82639d4806a643ce39d
jiwoncpark <jiwon.christine.park@gmail.com>
2020-12-22 12:44:46 -0800

    Make some cosmetic changes

--- hierarc/Likelihood/LensLikelihood/ddt_hist_likelihood.py
+++ hierarc/Likelihood/LensLikelihood/ddt_hist_likelihood.py
@@ -14,7 +14,9 @@ class DdtHistLikelihood(object):
     original source: https://github.com/shsuyu/H0LiCOW-public/blob/master/H0_inference_code/lensutils.py
     credits to Martin Millon, Aymeric Galan
     """
-    def __init__(self, z_lens, z_source, ddt_samples, kde_kernel=None, ddt_weights=None, bandwidth=20, nbins_hist=200,
+    def __init__(self, z_lens, z_source, 
+                 ddt_samples, kde_kernel=None, ddt_weights=None, 
+                 bandwidth=20, nbins_hist=200, 
                  normalized=False, binning_method=None):
         """
 
@@ -25,28 +27,36 @@ class DdtHistLikelihood(object):
         :param kde_kernel: string of KDE kernel type
         :param bandwidth: bandwith of kernel
         :param nbins_hist: number of bins in the histogram
-        :param normalized: bool, if True, returns the normalized likelihood, if False, separates the constant prefactor
-         (in case of a Gaussian 1/(sigma sqrt(2 pi)) ) to compute the reduced chi2 statistics
-        :binning_method: method used to calculate the bandwidth. "scott", "silverman", and a scalar constant (KDE factor) are supported. (See the scipy.stats.gaussian_kde documentation for details.)
+        :param normalized: bool, if True, returns the normalized likelihood, 
+        if False, separates the constant prefactor (in case of a Gaussian 
+        1/(sigma sqrt(2 pi))) to compute the reduced chi2 statistics
+        :binning_method: method used to calculate the bandwidth. "scott", 
+        "silverman", and a scalar constant (KDE factor) are supported. 
+        (See the scipy.stats.gaussian_kde documentation for details.)
         """
         if binning_method is None:
-            hist = np.histogram(ddt_samples, bins=nbins_hist, weights=ddt_weights)
+            hist = np.histogram(ddt_samples, 
+                                bins=nbins_hist, 
+                                weights=ddt_weights)
             vals = hist[0]
-            bins = [(h + hist[1][i + 1]) / 2.0 for i, h in enumerate(hist[1][:-1])]
-
+            bins = [(h + hist[1][i+1])/2.0 for i, h in enumerate(hist[1][:-1])]
             # ignore potential zero weights, sklearn does not like them
             kde_bins = [b for v, b in zip(vals, bins) if v > 0]
             kde_weights = [v for v in vals if v > 0]
-            self._kde = gaussian_kde(dataset=kde_bins, weights=kde_weights[:])
+            self._kde = gaussian_kde(dataset=kde_bins, 
+                                     weights=kde_weights[:])
         else:
-            self._kde = gaussian_kde(dataset=ddt_samples, bw_method=binning_method, weights=ddt_weights)
+            self._kde = gaussian_kde(dataset=ddt_samples, 
+                                     bw_method=binning_method, 
+                                     weights=ddt_weights)
         self.num_data = 1
         self._sigma = np.std(ddt_samples)
         self._norm_factor = 0
         if normalized is False:
             self._norm_factor = np.log(1. / self._sigma / np.sqrt(2*np.pi))
         self._ddt_mean = np.average(ddt_samples, weights=ddt_weights)
-        variance = np.average((ddt_samples - self._ddt_mean) ** 2, weights=ddt_weights)
+        variance = np.average((ddt_samples - self._ddt_mean) ** 2, 
+                              weights=ddt_weights)
         self._ddt_sigma = math.sqrt(variance)
 
     def log_likelihood(self, ddt, dd=None):
