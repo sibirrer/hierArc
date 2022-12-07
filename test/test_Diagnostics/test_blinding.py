616587951b5ad25baf4a6300c39f4a19a3165dbd
Simon Birrer <sibirrer@users.noreply.github.com>
2022-12-07 15:32:57 -0800

    fix tests mean -> median

--- test/test_Diagnostics/test_blinding.py
+++ test/test_Diagnostics/test_blinding.py
@@ -8,6 +8,6 @@ def test_blind_posterior():
     posterior = np.random.random((100, 3))
     posterior_blinded = blind_posterior(posterior=posterior, param_names=param_names)
     assert len(posterior[:, 0]) == 100
-    npt.assert_almost_equal(np.mean(posterior_blinded[:, 0]), 70, decimal=8)
-    npt.assert_almost_equal(np.mean(posterior_blinded[:, 1]), 1, decimal=8)
+    npt.assert_almost_equal(np.median(posterior_blinded[:, 0]), 70, decimal=8)
+    npt.assert_almost_equal(np.median(posterior_blinded[:, 1]), 1, decimal=8)
 
