2746c7b3e3b63fd079f9539e7d0da93344684a06
William Sheu <wiliam.sheu@berkeley.edu>
2024-01-09 13:24:56 -0800

    increased test coverage

--- test/test_LensPosterior/test_kin_constraints_composite.py
+++ test/test_LensPosterior/test_kin_constraints_composite.py
@@ -839,6 +839,82 @@ class TestRaise(unittest.TestCase):
                 **kwargs_kin_api_settings
             )
 
+        with self.assertRaises(ValueError):
+            anisotropy_model = "FAKE_MODEL"
+            kwargs_aperture = {
+                "aperture_type": "shell",
+                "r_in": 0,
+                "r_out": 3 / 2.0,
+                "center_ra": 0.0,
+                "center_dec": 0,
+            }
+            kwargs_seeing = {"psf_type": "GAUSSIAN", "fwhm": 1.4}
+
+            # numerical settings (not needed if power-law profiles with Hernquist light distribution is computed)
+            kwargs_numerics_galkin = {
+                "interpol_grid_num": 100,  # numerical interpolation, should converge -> infinity
+                "log_integration": True,
+                # log or linear interpolation of surface brightness and mass models
+                "max_integrate": 100,
+                "min_integrate": 0.001,
+            }  # lower/upper bound of numerical integrals
+
+            # redshift
+            z_lens = 0.5
+            z_source = 1.5
+            theta_E = 1.0
+            r_eff = 1
+            gamma = 2.1
+
+            kwargs_mge_light = {
+                "grid_spacing": 0.1,
+                "grid_num": 10,
+                "n_comp": 20,
+                "center_x": 0,
+                "center_y": 0,
+            }
+
+            # settings for kinematics calculation with KinematicsAPI of lenstronomy
+            kwargs_kin_api_settings = {
+                "multi_observations": False,
+                "kwargs_numerics_galkin": kwargs_numerics_galkin,
+                "kwargs_mge_light": kwargs_mge_light,
+                "sampling_number": 50,
+                "num_kin_sampling": 50,
+                "num_psf_sampling": 50,
+            }
+
+            kwargs_lens_light = [{"Rs": r_eff * 0.551, "amp": 1.0}]
+            gamma_in_array = np.linspace(0.1, 2.9, 5)
+
+            log_m2l_array = np.random.uniform(0.1, 1, 5)
+            rho0_array = 10 ** np.random.normal(8, 0.2, 5) / 1e6
+            r_s_array = np.random.normal(0.1, 0.01, 5)
+
+            KinConstraintsComposite(
+                z_lens=z_lens,
+                z_source=z_source,
+                gamma_in_array=gamma_in_array,
+                log_m2l_array=log_m2l_array,
+                kappa_s_array=rho0_array,
+                r_s_angle_array=r_s_array,
+                theta_E=theta_E,
+                theta_E_error=0.01,
+                gamma=gamma,
+                gamma_error=0.02,
+                r_eff=r_eff,
+                r_eff_error=0.05,
+                sigma_v_measured=[200],
+                sigma_v_error_independent=[10],
+                sigma_v_error_covariant=0,
+                kwargs_aperture=kwargs_aperture,
+                kwargs_seeing=kwargs_seeing,
+                anisotropy_model=anisotropy_model,
+                kwargs_lens_light=kwargs_lens_light,
+                is_m2l_population_level=False,
+                **kwargs_kin_api_settings
+            )
+
 
 if __name__ == "__main__":
     pytest.main()
