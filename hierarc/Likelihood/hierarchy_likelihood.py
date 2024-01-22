6f8730477975e460256a94be59c054b83a05d93a
William Sheu <wiliam.sheu@berkeley.edu>
2024-01-22 15:56:30 -0800

    fixed log likelihood

--- hierarc/Likelihood/hierarchy_likelihood.py
+++ hierarc/Likelihood/hierarchy_likelihood.py
@@ -283,9 +283,9 @@ class LensLikelihood(TransformedCosmography, LensLikelihoodBase, ParameterScalin
 
         if self._gamma_in_prior is not None and self._gamma_in_error is not None:
             if self._gamma_in_array is not None and self._log_m2l_array is not None:
-                lnlikelihood -= (self._gamma_in_prior - scaling_param_array[-2])**2 / (2 * self._gamma_in_error)
+                lnlikelihood -= (self._gamma_in_prior - scaling_param_array[-2])**2 / (2 * self._gamma_in_error**2)
             elif self._gamma_in_array is not None and self._log_m2l_array is None:
-                lnlikelihood -= (self._gamma_in_prior - scaling_param_array[-1])**2 / (2 * self._gamma_in_error)
+                lnlikelihood -= (self._gamma_in_prior - scaling_param_array[-1])**2 / (2 * self._gamma_in_error**2)
 
         return np.nan_to_num(lnlikelihood)
 
