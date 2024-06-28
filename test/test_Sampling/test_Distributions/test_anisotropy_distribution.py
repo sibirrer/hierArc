caa13ded9678e8b290e67a028f2cbcdbfde4b24b
Simon Birrer <sibirrer@users.noreply.github.com>
2024-06-27 21:50:29 -0400

    improve testing and stability

--- test/test_Sampling/test_Distributions/test_anisotropy_distribution.py
+++ test/test_Sampling/test_Distributions/test_anisotropy_distribution.py
@@ -1,3 +1,4 @@
+import numpy.testing as npt
 from hierarc.Sampling.Distributions.anisotropy_distributions import (
     AnisotropyDistribution,
 )
@@ -47,3 +48,44 @@ class TestAnisotropyDistribution(object):
         )
         kwargs_drawn = ani_dist.draw_anisotropy()
         assert "a_ani" not in kwargs_drawn
+
+        ani_dist = AnisotropyDistribution(
+            anisotropy_model="GOM",
+            anisotropy_sampling=True,
+            distribution_function="NONE",
+            kwargs_anisotropy_min=None,
+            kwargs_anisotropy_max=None,
+        )
+        kwargs_drawn = ani_dist.draw_anisotropy(a_ani=1, beta_inf=0.9)
+        assert kwargs_drawn["a_ani"] == 1
+        assert kwargs_drawn["beta_inf"] == 0.9
+
+        kwargs_anisotropy = {
+            "a_ani": 1,
+            "beta_inf": 0.8,
+            "a_ani_sigma": 2,
+            "beta_inf_sigma": 2,
+        }
+
+        for i in range(100):
+            kwargs_drawn = self._ani_dist.draw_anisotropy(**kwargs_anisotropy)
+
+    def test_raises(self):
+
+        with npt.assert_raises(ValueError):
+            kwargs_anisotropy = {
+                "a_ani": -1,
+                "beta_inf": 0.8,
+                "a_ani_sigma": 0.1,
+                "beta_inf_sigma": 0.2,
+            }
+            kwargs_drawn = self._ani_dist.draw_anisotropy(**kwargs_anisotropy)
+
+        with npt.assert_raises(ValueError):
+            kwargs_anisotropy = {
+                "a_ani": 1,
+                "beta_inf": -1,
+                "a_ani_sigma": 0.1,
+                "beta_inf_sigma": 0.2,
+            }
+            kwargs_drawn = self._ani_dist.draw_anisotropy(**kwargs_anisotropy)
