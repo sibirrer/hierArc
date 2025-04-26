fixed bug in kin scaling, and made interp2d be replaced with updated method

hierarc/Likelihood/LensLikelihood/ddt_dd_kde_likelihood.py
@@ -1,6 +1,7 @@
 from lenstronomy.Cosmo.kde_likelihood import KDELikelihood
 import numpy as np
-from scipy import interpolate
+#from scipy import interpolate
+from scipy.interpolate import RectBivariateSpline
 
 
 class DdtDdKDELikelihood(object):
@@ -46,8 +47,8 @@ class DdtDdKDELikelihood(object):
             for i, dd in enumerate(dd_grid):
                 for j, ddt in enumerate(ddt_grid):
                     z[j, i] = self._kde_likelihood.logLikelihood(dd, ddt)[0]
-            self._interp_log_likelihood = interpolate.interp2d(
-                dd_grid, ddt_grid, z, kind="cubic"
+            self._interp_log_likelihood = RectBivariateSpline(
+                dd_grid, ddt_grid, z.T
             )
         self._interpol = interpol
         self.num_data = 2
@@ -67,5 +68,5 @@ class DdtDdKDELikelihood(object):
         else:
             dd_ = dd
         if self._interpol is True:
-            return self._interp_log_likelihood(dd_, ddt)[0]
+            return self._interp_log_likelihood(dd_, ddt).T[0]
         return self._kde_likelihood.logLikelihood(dd_, ddt)[0]

hierarc/Likelihood/kin_scaling.py
@@ -1,7 +1,7 @@
 __author__ = "sibirrer", "ajshajib"
 
 from scipy.interpolate import interp1d
-from scipy.interpolate import interp2d
+from scipy.interpolate import RectBivariateSpline
 from scipy.interpolate import RegularGridInterpolator
 import numpy as np
 
@@ -90,9 +90,10 @@ class ParameterScalingSingleMeasurement(object):
                     fill_value="extrapolate",
                 )
             elif self._dim_scaling == 2:
-                self._f_ani = interp2d(
-                    param_grid_axes[0], param_grid_axes[1], j_kin_scaling_grid.T
-                )
+                self._f_ani = r = RectBivariateSpline(param_grid_axes[0], param_grid_axes[1], j_kin_scaling_grid)
+                #self._f_ani = interp2d(
+                #    param_grid_axes[0], param_grid_axes[1], j_kin_scaling_grid.T
+                #)
             else:
                 self._f_ani = RegularGridInterpolator(
                     tuple(param_grid_axes),
@@ -111,7 +112,7 @@ class ParameterScalingSingleMeasurement(object):
         if self._dim_scaling == 1:
             return self._f_ani(param_array[0])
         elif self._dim_scaling == 2:
-            return self._f_ani(param_array[0], param_array[1])[0]
+            return self._f_ani(param_array[0], param_array[1])[0].T[0]
         else:
             return self._f_ani(param_array)[0]
 

hierarc/Sampling/Distributions/los_distributions.py
@@ -82,7 +82,7 @@ class LOSDistribution(object):
                     "line of sight distribution %s not valid." % self._los_distribution
                 )
         else:
-            kappa_ext_draw = 0
+            kappa_ext_draw = np.zeros(size)
         return kappa_ext_draw
 
     def draw_bool(self, kwargs_los):