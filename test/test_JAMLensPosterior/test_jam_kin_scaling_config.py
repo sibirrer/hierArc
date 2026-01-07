7b4cefdebf6a111ebd3fa9ea12ba1869cf54d9ff
Felipe Urcelay <fjurcelay@uc.cl>
2026-01-07 15:32:29 +0000

    jam_kin_scaling_config.py and test

--- test/test_JAMLensPosterior/test_jam_kin_scaling_config.py
+++ test/test_JAMLensPosterior/test_jam_kin_scaling_config.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-from hierarc.LensPosterior.kin_scaling_config import KinScalingConfig
-import numpy.testing as npt
+from hierarc.JAMLensPosterior.jam_kin_scaling_config import JAMKinScalingConfig
 
 
 class TestKinScalingConfig(object):
@@ -9,24 +8,22 @@ class TestKinScalingConfig(object):
     def setup_method(self):
         pass
 
-    def test_kwargs_lens_base(self):
-        kin_scaling = KinScalingConfig(
+    def test_kwargs_axisymmetric(self):
+        kin_scaling = JAMKinScalingConfig(
             anisotropy_model="GOM",
             r_eff=1,
             gamma_pl_scaling=np.linspace(1.5, 2.5, 5),
-            log_m2l_scaling=np.linspace(0, 1, 5),
-            gamma_in_scaling=np.linspace(0.5, 1.5, 5),
+            q_intrinsic_scaling=np.linspace(0.4, 1.0, 6),
+            q_intrinsic_mean=0.8,
         )
-        kin_scaling.kwargs_lens_base
+        assert kin_scaling.num_scaling_dim == 4  # gamma_pl, q_intrinsic, a_ani, beta_inf
 
-    def test_init(self):
-        kin_scaling = KinScalingConfig(
-            anisotropy_model="NONE",
-            r_eff=None,
-            gamma_in_scaling=None,
-            log_m2l_scaling=None,
+        assert kin_scaling.axisymmetric_jam_base['q_intrinsic'] == 0.8
+
+        kin_scaling = JAMKinScalingConfig(
+            anisotropy_model="GOM",
+            r_eff=1,
+            gamma_pl_scaling=np.linspace(1.5, 2.5, 5),
+            q_intrinsic_scaling=np.linspace(0.5, 1.0, 6),
         )
-        kin_scaling._anisotropy_model = "BAD"
-
-        with npt.assert_raises(ValueError):
-            kin_scaling.anisotropy_kwargs()
+        assert kin_scaling.axisymmetric_jam_base['q_intrinsic'] == 0.75
