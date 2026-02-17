41ca385fcfd347d5833039444dc4f8bd5ec0f4d7
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2026-02-17 06:15:19 +0100

    Add support for AstroPy 7

--- hierarc/Sampling/ParamManager/cosmo_model.py
+++ hierarc/Sampling/ParamManager/cosmo_model.py
@@ -3,8 +3,12 @@ from astropy.cosmology import FLRW, FlatFLRWMixin
 from scipy.special import exp1
 from astropy.cosmology.core import dataclass_decorator
 from astropy.cosmology.parameter import Parameter
-from astropy.cosmology._utils import aszarr
-
+try:
+    # AstroPy 7
+    from astropy.cosmology._src.utils import aszarr
+except:
+    # AstroPy 6
+    from astropy.cosmology._utils import aszarr
 
 @dataclass_decorator
 class wPhiCDM(FlatFLRWMixin, FLRW):
